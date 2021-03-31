7eb1e82ba57554e506c0ccdfa0ded0f4f2343721
marcelo vinicius <viniciusmarcello12@gmail.com>
2021-03-30 22:20:58 -0300

    criação da tranformação do valor de acordo com a cotação atual do dolar

---
+++ js/funcoes.js
@@ -0,0 +1,14 @@
+let valorEmDolarTexto = prompt("qual o valor em dolar voce quer converter?");
+console.log( typeof valorEmDolarTexto);
+//string = texto
+let valorEmDolarNumero = parseFloat(valorEmDolarTexto);
+console.log(typeof valorEmDolarNumero );
+//float = número decimal 
+//int = numero inteiro
+
+alert(valorEmDolarNumero);
+
+let valorEmReal = valorEmDolarNumero * 5.77;
+
+let valorEmRealFixado = valorEmReal.toFixed(2); 
+alert(valorEmRealFixado);
